03f531eb3a53e7902636b49f4b8922b1af1ae2f7
Evgeny372828 <zenas7160@gmail.com>
2023-11-12 16:45:20 +0300

    com3

--- Project/main.py
+++ Project/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from .database import Base
 app = FastAPI()
+
+class Company(Base):
+    __tablename__="company"
+
+    id=Column(Integer,primary_key=True,index=True)
+    name_company=Column(String,unique=True, index=True)
+
+
 
 
 @app.get("/")
